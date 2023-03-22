58cf44a9e1513e7182fb79b0ab1d5e7c20377008
Liam DeVoe <orionldevoe@gmail.com>
2023-03-22 13:43:58 -0400

    checkpoint 03/22/2023

--- tybug-local/AppDelegate.swift
+++ tybug-local/AppDelegate.swift
@@ -6,13 +6,18 @@ import HotKey
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     static let dataDirPath = NSHomeDirectory() + "/Library/Application Support/tybug-local"
-    let searcher = Searcher()
+    let searcher: Searcher
 
     override init() {
         // ensure data dir exists
         if !FileManager.default.fileExists(atPath: Self.dataDirPath) {
             try! FileManager.default.createDirectory(atPath: Self.dataDirPath, withIntermediateDirectories: false)
         }
+
+        // initiailize here instead of at the top level so we ensure that any AppDelegate initialization has
+        // occurred. For instance, we want to ensure that the dataDirPath has been created, or searcher will
+        // error when trying to access the db.
+        searcher = Searcher()
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
